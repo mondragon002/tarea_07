857f5e79c33a0ab14fe20fd8e96ab1221eb799af
Jose Miguel <josemiguelcondormondragon@gmail.com>
2025-05-23 11:45:48 -0500

    first commit

---
+++ Views/Formulario.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<form action="index.php" method="post">
+  <label for="nombre">Nombre:</label><br>
+  <input type="text" id="nombre" name="nombre" required><br><br>
+
+  <label for="correo">Correo:</label><br>
+  <input type="email" id="correo" name="correo" required><br><br>
+
+  <label for="comentarios">Comentarios:</label><br>
+  <textarea id="comentarios" name="comentarios" rows="4" required></textarea><br><br>
+
+  <button type="submit">Enviar</button>
+</form>
---
+++ index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Formulario de Contacto</title>
+</head>
+<body>
+  <h1>Bienvenido al Formulario</h1>
+
+  <?php
+ 
+    require 'views/Formulario.php';
+  ?>
+
+</body>
+</html>
